0bfbc99009ec2538c8d8a27b1e8cdf44db1c7b8f
Tobias Lidskog <tobias@lidskog.se>
2018-04-12 18:08:59 +0200

    Simple test file is too simple to be helpful.

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,6 @@
 import test from 'ava';
 import { parseTracelog } from './_helpers';
 
-test('Can find main thread', async t => {
-  const parsed = await parseTracelog('simple.json');
-  t.log(parsed);
-  t.is(parsed.mainThread, '2343:2347');
-});
-
 test('Can find main thread in complex file', async t => {
   const parsed = await parseTracelog('www.google.ru.json');
   t.log(parsed);
