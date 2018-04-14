27103f254fa7da6e735373ac9d5b750d7a04b2a3
Tobias Lidskog <tobias@lidskog.se>
2018-04-14 19:24:00 +0200

    Remove logging from tests.
    Reduce terminal spam…

--- test/tests.js
+++ test/tests.js
@@ -3,13 +3,11 @@ import { parseTracelog } from './_helpers';
 
 test('Can find main thread in complex file', async t => {
   const parsed = await parseTracelog('www.google.ru.json');
-  t.log(parsed);
   t.is(parsed.mainThread, '69940:775');
 });
 
 test('Can find main thread in www.sitespeed.io file', async t => {
   const parsed = await parseTracelog('www.sitespeed.io.json');
-  t.log(parsed);
   t.is(parsed.mainThread, '64513:775');
 });
 
