cad1618bd2e2c74e831aa8fa83f0c9ac3bfac263
Tobias Lidskog <tobias@lidskog.se>
2018-04-30 13:37:07 +0200

    Fix typo in name of constant.

--- test/_helpers.js
+++ test/_helpers.js
@@ -4,10 +4,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 import parser from '../';
 
-const TRACEFLOGSPATH = path.resolve(__dirname, 'fixtures', 'tracelogs');
+const TRACELOGS_PATH = path.resolve(__dirname, 'fixtures', 'tracelogs');
 
 export async function parseTracelog(filename, options = {}) {
   return parser.parseStream(
-    fs.createReadStream(path.resolve(TRACEFLOGSPATH, filename), options)
+    fs.createReadStream(path.resolve(TRACELOGS_PATH, filename), options)
   );
 }
