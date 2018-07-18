050211c2a1179d03728f7826d1b2bcfecf155320
soulgalore <peter@soulgalore.com>
2018-07-18 11:40:19 +0200

    publish the version to the docs

--- release.sh
+++ release.sh
@@ -5,3 +5,7 @@ set -e
 # npm install --global np
 
 np $1
+
+PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')
+
+echo $PACKAGE_VERSION  > ../sitespeed.io/docs/_includes/version/chrome-trace.txt
